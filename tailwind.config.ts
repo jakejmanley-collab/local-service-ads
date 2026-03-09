21616839765d4380c79739d3fb43d1dafc797ed0
jakejmanley-collab <jakejmanley@gmail.com>
2026-03-09 12:15:30 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,8 @@ const config: Config = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'), // THIS IS THE MAGIC LINE
+  ],
 }
 export default config
