689a153a0d82206d7f5a0533ebbccf59879a0542
jakejmanley-collab <jakejmanley@gmail.com>
2026-03-09 12:20:40 -0400

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography' // <-- We import it here instead
 
 const config: Config = {
   content: [
@@ -10,7 +11,7 @@ const config: Config = {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/typography'), // THIS IS THE MAGIC LINE
+    typography, // <-- And use it here
   ],
 }
 export default config
